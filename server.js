03c12ea388d4f4397207bd7ce41b7be0ca6e1eae
generalmeow <rcbrown@gmail.com>
2017-01-16 11:57:36 -0800

    set up server

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
+// TODO: create a basic server with express
+// that will send back the index.html file on a GET request to '/'
+// it should then send back jsonData on a GET to /data
 var express = require('express');
 var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
 
-app.get('/', function(req, res){
-  // res.sendFile takes an absolute path to a file and
-  // sets the mime type based n the file extname
-  res.sendFile(__dirname + '/index.html', function(err) {
-    if (err) {
-      res.status(500).send(err);
+app.get('/', function(request, response){
+  response.setHeader('Content-Type', 'text/html')
+  response.sendFile(__dirname, '/index.html', function(err){
+    if(err){
+      response.status(500).send(err)
     }
   })
+})
+
+app.get('/data', function(request, response){
+  response.json(jsonData);
 });
 
-app.get('/data', function(req, res) {
-  res.json(jsonData);
-});
-
-
-var port = 3000;
+var port = 3000
 app.listen(port, function(){
-  console.log('listening on http://localhost:', port);
+  console.log('listening on port 3000!');
 });
