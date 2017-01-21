a38cd9c1c773214ee2980ceb441d011dfc325f96
generalmeow <rcbrown@gmail.com>
2017-01-21 14:57:24 -0800

    setting up tigers

---
+++ server.js
@@ -0,0 +1,25 @@
+// TODO: create a basic server with express
+// that will send back the index.html file on a GET request to '/'
+// it should then send back jsonData on a GET to /data
+var express = require('express');
+var app = express();
+
+var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(request, response){
+  response.setHeader('Content-Type', 'text/html')
+  response.sendFile(__dirname, '/index.html', function(err){
+    if(err){
+      response.status(500).send(err)
+    }
+  })
+})
+
+app.get('/data', function(request, response){
+  response.json(jsonData);
+});
+
+var port = 3000
+app.listen(port, function(){
+  console.log('------>>>>>listening on port 3000!');
+});
--- server/tigers.js
+++ server/tigers.js
@@ -2,3 +2,49 @@
 // and make some REST routes for it, exactly like for lions
 // make a middleware that just logs the word 'tiger' to the console
 // when a request comes in to the server
+
+const tigerRouter = require('express').Router();
+
+const lions = [];
+const id = 0;
+// id generating function
+const updateId = function( req, res, next ) {
+  if( !req.body.id ) {
+    id++;
+    req.body.id = id + '';
+  }
+  next();
+}
+
+tigerRouter.param( 'id', function( req, res, next, id ) {
+  const todo = _.find(todos, { id: id } )
+
+  if( todo ) {
+    req.todo = todo
+    next()
+  } else {
+    res.send()
+  }
+} )
+
+const tigerMiddleware = () => {
+  return console.log('tigers')
+}
+
+tigerRouter.get('/', function( req, res, next ) {
+  res.send( tigerMiddleware() );
+})
+
+tigerRouter.get('/:id', function( req, res, next ) {
+  res.send( tigerMiddleware() )
+})
+
+tigerRouter.post('/', updateId, function( req, res, next ) {
+  res.send( tigerMiddleware() )
+})
+
+tigerRouter.put('/:id', function( req, res, next ) {
+  res.send( tigerMiddleware() )
+})
+
+module.exports = tigerRouter;
